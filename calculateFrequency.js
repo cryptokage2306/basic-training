cc6e7458ea32c72847198a4f77f36ea48085d551
Vivek <vjain@techracers.io>
2019-03-22 18:02:20 +0530

    correct indentation

--- calculateFrequency.js
+++ calculateFrequency.js
@@ -1,18 +1,14 @@
 function calculateFrequency(string) {
-    let toReturn ={};
+    let toReturn = {};
     for (let i = 0; i < string.length; i++) {
-        if(string.charCodeAt(i)>96 && string.charCodeAt(i)<123){
-            toReturn[string.charAt(i)]=0;
+        if (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123) {
+            toReturn[string.charAt(i)] = 0;
         }
-        
-        
     }
     for (let i = 0; i < string.length; i++) {
-        if(string.charCodeAt(i)>96 && string.charCodeAt(i)<123){
-            toReturn[string.charAt(i)]=toReturn[string.charAt(i)]+1;
+        if (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123) {
+            toReturn[string.charAt(i)] = toReturn[string.charAt(i)] + 1;
         }
-        
     }
     return toReturn;
-  }
-  console.log(calculateFrequency("u@#d$fkj$#!kds"));
+}
--- flatten.js
+++ flatten.js
@@ -1,29 +1,17 @@
-
-
 function flatten(unflatObject) {
-    var toReturn = {};
-      count=0;
-      for (var i in unflatObject) {
-          
-          if ((typeof unflatObject[i]) == 'object') {
-              
-              var flatObject = flatten(unflatObject[i]);
-              
-              for (var x in flatObject) {
-                  
-                  
-                  toReturn[i + '.' + x] = flatObject[x];
-                  
-              }
-              
-          } else {
-              
-              toReturn[i] = unflatObject[i];
-              
-          }
-      }
-      return toReturn;
-  }
+    var toReturn = {},
+        count = 0;
+    for (var i in unflatObject) {
+        if ((typeof unflatObject[i]) == 'object') {
+            var flatObject = flatten(unflatObject[i]);
+            for (var x in flatObject) {
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = unflatObject[i];
+        }
+    }
+    return toReturn;
+}
 
 
-console.log(flatten({ "flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] }));
