6bb4d9eb6dd3fcf86a6fa81fadb43108d8bf5a80
vivek jain <vkjvivek7@gmail.com>
2019-04-06 14:17:23 +0530

    corrct Indentation

--- array5.js
+++ array5.js
@@ -1,26 +1,23 @@
 const fs = require('fs');
-let j=0;
+let j = 0;
 console.log("====================================");
 function f1(arr) {
-  if(j<arr.length){
-    
-    fs.readFile(arr[j],function(err,data){
-      if(err){
+  if (j < arr.length) {
+    fs.readFile(arr[j], function (err, data) {
+      if (err) {
         console.log(err);
-      }else{
+      } else {
         console.log(data.toString());
         j++;
         f1(arr);
         console.log("====================================");
-      } 
+      }
     });
   }
-  
 }
 var arr = []
 for (var i = 0; i < 5; i++) {
   arr.push(`/home/asd/nodejs_training/txtfiles/${i}.txt`);
 }
-
 f1(arr);
 
