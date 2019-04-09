f3e6f8cc2c3f34d0b8874d5f9e44f09d803cdb3f
vivek jain <vkjvivek7@gmail.com>
2019-04-09 22:53:56 +0530

    Indentation Correction from 4 to 2 spaces and blank line removal btw code

--- secondLargest.js
+++ secondLargest.js
@@ -1,25 +1,25 @@
 function secondLargest(array) {
-    let v = [];
-    v.length = 1000;
-    for (let i = 0; i < v.length; i++) {
-        v[i] = 0;
+  let v = [];
+  v.length = 1000;
+  for (let i = 0; i < v.length; i++) {
+    v[i] = 0;
+  }
+  len = array.length;
+  for (let i = 0; i < array.length; i++) {
+    if (v[array[i]] == 1) {
+      len = len - 1;
+    } else {
+      v[array[i]] = 1;
     }
-    len = array.length;
-    for (let i = 0; i < array.length; i++) {
-        if (v[array[i]] == 1) {
-            len = len - 1;
-        } else {
-            v[array[i]] = 1;
-        }
+  }
+  let i = 0,
+    count = 0;
+  while (count < len - 1) {
+    if (v[i] == 1) {
+      count = count + 1;
     }
-    let i = 0,
-        count = 0;
-    while (count < len - 1) {
-        if (v[i] == 1) {
-            count = count + 1;
-        }
-        i++;
-    }
-    return i - 1;
+    i++;
+  }
+  return i - 1;
 }
 
