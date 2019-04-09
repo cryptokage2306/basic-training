3ef53149a8300795858d1f478a484a3ff1ba051e
vivek jain <vkjvivek7@gmail.com>
2019-04-09 13:40:58 +0530

    String input to Final Output is integrated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const bodyParser=require('body-parser')
 const app = express();
 const tax=require('./sales_imported');
+const text_tax=require('./sales_imported_text');
 app.use("/",bodyParser.json());
 app.use("/",bodyParser.text())
 app.post('/',function(req,res,next){
@@ -32,9 +33,39 @@ app.post('/',function(req,res,next){
     console.log(2121212);
     next();
   }
-  
 },function(req,res){
   let str=req.body;
-  
+  console.log(str);
+  orderNo=str.split(":");
+  sentences=orderNo[1].split("\n");
+  console.dir(sentences);
+  quantity=[];
+  body=[];
+  price=[];
+  for(let i=1;i<sentences.length;i++){
+    regex=new RegExp('(\\d+)');
+    temp=sentences[i].split(regex);
+    quantity.push(temp[1]);
+    lastind=temp[2].lastIndexOf("at");
+    body.push(temp[2].substr(0,lastind)+":");
+    price.push(temp[3]+temp[4]+temp[5]);
+  }
+  stmt=[];
+  total=0;
+  sales=0
+  for(let i=1;i<sentences.length;i++){
+    sales+=text_tax.sales_tax(quantity[i-1],body[i-1],price[i-1])+text_tax.imported_tax(quantity[i-1],body[i-1],price[i-1]);
+    aftertax=parseFloat(price[i-1])+text_tax.sales_tax(quantity[i-1],body[i-1],price[i-1])+text_tax.imported_tax(quantity[i-1],body[i-1],price[i-1]);
+    stmt.push(`${quantity[i-1]} ${body[i-1]} ${aftertax}`);
+    total+=aftertax;
+  }
+  res.json({
+    "name":orderNo[0],
+    "stmt":stmt,
+    "total":total,
+    "sales":sales
+  });
+
+  res.end("hello");
 });
 app.listen(3000);
