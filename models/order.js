ae69150e690b052012c8b283c58ed46a34ed8a99
vivek jain <vkjvivek7@gmail.com>
2019-04-15 15:13:06 +0530

    Indent Correction

--- models/order.js
+++ models/order.js
@@ -2,18 +2,28 @@
 module.exports = (sequelize, DataTypes) => {
   const order = sequelize.define('order', {
     orderName: DataTypes.STRING,
-    itemName:{type:DataTypes.STRING,
-    allowNull:false},
-    itemCategory:{type:DataTypes.STRING,
-    allowNull:false},
-    quantity:{type:DataTypes.INTEGER,
-    allowNull:false},
-    price:{type:DataTypes.FLOAT,
-    allowNull:false},
-    imported:{type:DataTypes.STRING,
-    allowNull:true},
+    itemName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    itemCategory: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    imported: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
   }, {});
-  order.associate = function(models) {
+  order.associate = function (models) {
     // associations can be defined here
   };
   return order;
